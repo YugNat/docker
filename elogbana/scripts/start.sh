2c512c7e7148ab971a3022a4b2c41b894f2f88eb
yugnat <tanguy.sagot@gmail.com>
2015-09-14 15:22:31 +0200

    Upgrade to new version

--- elogbana/scripts/start.sh
+++ elogbana/scripts/start.sh
@@ -1,5 +1,5 @@
-/opt/elasticsearch-1.7.0/bin/elasticsearch &
-/opt/logstash-1.5.2/bin/logstash agent -f /opt/logstash-1.5.2/conf/logstash.conf &
+/opt/elasticsearch-1.7.1/bin/elasticsearch &
+/opt/logstash-1.5.4/bin/logstash agent -f /opt/logstash-1.5.4/conf/logstash.conf &
 sleep 5
 /opt/kibana/bin/kibana &
 /bin/bash
